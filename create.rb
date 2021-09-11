cdb0a6916718c67142f14843e04d6dd4f6b076a5
victor <victorg.rr2005@gmail.com>
2021-09-10 21:57:49 -0300

    repo init

---
+++ create.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+print "Nome do projeto: "
+name = gets.chomp
+
+print "Ferramentas: "
+ferra = gets.chomp
+
+print "Objetivos gerais: "
+gerOb = gets.chomp
+
+print "Objetivos específicos: "
+espOb = gets.chomp.encode("UTF-8").split("/")
+
+
+File.open("#{name}.md", "w+", :encoding => 'utf-8') do |f|
+    f.write("# PROJETO #{name.upcase}\n")
+
+    f.write("\n## Ferramentas: \n")
+    f.write(ferra)
+
+    f.write("\n\n\n## Objetivo geral: \n")
+    f.write(gerOb)
+
+    f.write("\n\n\n## Objetivos específicos:")
+    espOb.each do |first|
+        first.split("-").each do |second|
+            
+            if second == first.split("-")[0] 
+                f.write("\n- [ ] #{second}")
+            else 
+                f.write("\n    - [ ] #{second}")
+            end
+
+        end
+    end
+
+end
